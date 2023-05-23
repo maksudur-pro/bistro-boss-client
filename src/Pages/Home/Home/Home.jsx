4cfcc2ec499ebc9cc8f870ed8684233100192145
unknown <ajoy9532@gmail.com>
2023-05-24 01:11:03 +0600

    added featured

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import PopularMenu from "../PopularMenu/PopularMenu";
+import Featured from "../Featured/Featured";
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Category></Category>
       <PopularMenu></PopularMenu>
+      <Featured></Featured>
     </div>
   );
 };
