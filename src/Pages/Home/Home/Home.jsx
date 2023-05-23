63dbf6e4c55f1e4cde2d00cb636f664a3bda7b7a
unknown <ajoy9532@gmail.com>
2023-05-24 00:37:16 +0600

    added popular menu

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import PopularMenu from "../PopularMenu/PopularMenu";
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Category></Category>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
