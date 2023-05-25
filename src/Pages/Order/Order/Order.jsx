477c0eedd26ce8a92665c53049a70bb65842382c
unknown <ajoy9532@gmail.com>
2023-05-25 19:09:24 +0600

    added order section

---
+++ src/Pages/Order/Order/Order.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import orderImg from "../../../assets/shop/banner2.jpg";
+import Cover from "../../Shared/Cover/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import useMenu from "../../../Hooks/useMenu";
+
+const Order = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = useMenu();
+
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+  const salad = menu.filter((item) => item.category === "salad");
+
+  return (
+    <div>
+      <Cover img={orderImg} title="order food"></Cover>
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soup</Tab>
+          <Tab>Dessert</Tab>
+          <Tab>Drink</Tab>
+        </TabList>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Order;
