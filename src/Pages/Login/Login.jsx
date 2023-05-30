7af45a2981d4c8d5fc89d01db59b00057a1e6b6b
unknown <ajoy9532@gmail.com>
2023-05-30 22:48:43 +0600

    added updateProfile

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,13 +5,17 @@ import {
   validateCaptcha,
 } from "react-simple-captcha";
 import { AuthContext } from "../../Provider/AuthProvider";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import Swal from "sweetalert2";
 
 const Login = () => {
   const [disabled, setDisabled] = useState(true);
   const { signIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
 
   useEffect(() => {
     loadCaptchaEnginge(6);
@@ -33,6 +37,7 @@ const Login = () => {
       .catch((error) => {
         console.log(error.message);
       });
+    navigate(from, { replace: true });
   };
 
   const handleValidate = (e) => {
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
   const {
     register,
+    reset,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const { createUser } = useContext(AuthContext);
+  const { createUser, updateUserProfile } = useContext(AuthContext);
+
+  const navigate = useNavigate();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -19,6 +23,16 @@ const SignUp = () => {
       .then((result) => {
         const loggedUser = result.user;
         console.log(loggedUser);
+        updateUserProfile(data.name, data.photoURL)
+          .then(() => {
+            console.log("user photo updated");
+            reset();
+            Swal.fire("Good job!", "User created successfully", "success");
+            navigate("/");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       })
       .catch((error) => console.log(error));
   };
@@ -53,6 +67,20 @@ const SignUp = () => {
                 />
                 {errors.name && (
                   <span className="text-error">Name is required</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("photoURL", { required: true })}
+                  placeholder="photo link"
+                  className="input input-bordered"
+                />
+                {errors.photoURL && (
+                  <span className="text-error">Photo URL is required</span>
                 )}
               </div>
               <div className="form-control">
