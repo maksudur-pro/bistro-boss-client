e18980f70b002d6797665bcff7fe3edc06662706
unknown <ajoy9532@gmail.com>
2023-06-03 01:41:09 +0600

    added jwt

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -11,6 +11,7 @@ import {
 } from "firebase/auth";
 import app from "../firebase/firebase.config";
 import { set } from "react-hook-form";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -50,7 +51,18 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      console.log(currentUser);
+      // get and set token
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", { email: currentUser.email })
+          .then((data) => {
+            console.log(data.data.token);
+            localStorage.setItem("access_token", data.data.token);
+          });
+      } else {
+        localStorage.removeItem("access_token");
+      }
+
       setLoading(false);
     });
     return () => {
